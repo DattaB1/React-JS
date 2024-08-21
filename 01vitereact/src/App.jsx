12b3fa7163975c725ed34d16c58d318f2833f778
DattaB1 <dattabalbudhe1@gmail.com>
2024-08-21 16:22:15 +0530

    BasicFisrtDay

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Testing from'./Testing'
+function App() {
+
+  return (
+    <>
+    <div className="h">
+    <h1>Hello This is React Project Using Vite</h1>
+    <Testing/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Testing.jsx
@@ -0,0 +1,9 @@
+function Testing()
+{
+    return(
+        <>
+        <h1>This is Testing Component </h1>
+        </>
+    )
+}
+export default Testing;
