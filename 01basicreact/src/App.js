fb22d8b26ee65076f7a72968cfa49d9cab1b8c08
DattaB1 <dattabalbudhe1@gmail.com>
2024-08-22 16:09:38 +0530

    React

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Test from './Test';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is our React app</h1>
+      <Test></Test>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Test.js
@@ -0,0 +1,9 @@
+function Test()
+{
+    return(
+        <div>
+            <h2>This is Our Test page And Test Component So we add in app</h2>
+        </div>
+    )
+}
+export default Test
